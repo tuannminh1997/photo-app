055d4b5091a8f43e641f036581d17a4ab97948ad
tuannminh1997 <tuannminh1997@gmail.com>
2021-06-09 21:34:43 +0700

    Test scripe in production

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,9 @@
 Rails.configuration.stripe = {
-  :publishable_key => ENV[:STRIPE_TEST_PUBLISHABLE_KEY],
-  :secret_key => ENV[:STRIPE_TEST_SECRET_KEY]
+  :publishable_key => Rails.application.credentials.development[:STRIPE_TEST_PUBLISHABLE_KEY],
+  :secret_key => Rails.application.credentials.development[:STRIPE_TEST_SECRET_KEY]
+  # :publishable_key => ENV['STRIPE_TEST_PUBLISHABLE_KEY'],
+  #
+  # :secret_key => ENV['STRIPE_TEST_SECRET_KEY']
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
